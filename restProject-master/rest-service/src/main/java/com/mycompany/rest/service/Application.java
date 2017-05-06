26a6afec2017811790f10da7f5c641b465b9f951
helenwu424 <w204082158@student.hccs.edu>
2017-05-05 21:05:37 -0500

    Project

---
+++ restProject-master/rest-service/src/main/java/com/mycompany/rest/service/Application.java
@@ -0,0 +1,15 @@
+package com.mycompany.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+
+@SpringBootApplication
+@ImportResource("service-springConfig.xml")
+public class Application {
+     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        
+    }
+}
